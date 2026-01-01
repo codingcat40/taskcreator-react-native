35ed08732f255ad13fd86b383e881e4b7e8bec7b
Naman Matoliya <naman@fieldproxy.com>
2026-01-01 23:04:36 +0530

    setup backend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
@@ -17,6 +19,10 @@ const styles = StyleSheet.create({
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+  const todos = useQuery(api.todos.getTodos)
+  console.log(todos)
+
   return (
     <View
       style={styles.container}
